caffec6e247b831de9489f139fecc7f0ab40546c
StrandedDev <strandedcrow7@gmail.com>
2024-10-21 23:36:04 +0600

    test

--- public/scripts/particles/app.js
+++ public/scripts/particles/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-container', "/public/scripts/particles/particles_config.json", function() {
+particlesJS.load('particles-container', "./particles_config.json", function() {
   console.log('Subhan Allah');
 });
